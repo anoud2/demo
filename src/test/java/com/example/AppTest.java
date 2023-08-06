ac31b8b71e72027b2d3abf09a55e99d0733b8108
anoud2 <anoud.j.alsaud@gmail.com>
2023-08-06 17:33:27 +0300

    Chrome Automation Test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,35 +1,77 @@
 package com.example;
 
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+import java.util.logging.Logger;
 public class AppTest {
-@Test
-    public void eightComponents() {
+
+    @Test
+    public void openChrome() {
+
+        Logger localLogger = Logger.getLogger(this.getClass().getName());
+
+
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.get("https://www.google.com");
 
         String title = driver.getTitle();
-        assertEquals("Web form", title);
+        assertEquals("Google", title);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30000000));
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        WebElement p = driver.findElement(By.name("q"));
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        p.sendKeys("Selenium");
         submitButton.click();
+        p = driver.findElement(By.name("q"));
+        p.clear();
+        p.sendKeys("foodics");
+        p.sendKeys(Keys.ENTER);
+        //counts everything 
+        WebElement resultsUI = driver.findElement(By.id("rso"));
+        List<WebElement> results = resultsUI.findElements(By.xpath("./child::*"));
+        
+        //page1
+        WebElement nav  = driver.findElement(By.id("bres"));
+        js.executeScript("arguments[0].scrollIntoView();", nav);
+        WebElement nxtbtn = driver.findElement(By.id("pnnext"));
+        nxtbtn.click();
 
-        WebElement message = driver.findElement(By.id("message"));
-        String value = message.getText();
-        assertEquals("Received!", value);
+        //page 2
+        WebElement pageA = driver.findElement(By.id("rso"));
+        List<WebElement> resultsA = pageA.findElements(By.xpath("./child::*"));
+        int countA = resultsA.size();
+        nav  = driver.findElement(By.id("bres"));
+        js.executeScript("arguments[0].scrollIntoView();", nav);
+        nxtbtn = driver.findElement(By.id("pnnext"));
+        nxtbtn.click();
+        //page 3
+        WebElement pageB = driver.findElement(By.id("rso"));
+        List<WebElement> resultsB = pageB.findElements(By.xpath("./child::*"));
+        int countB = resultsB.size();
+        nav  = driver.findElement(By.id("bres"));
+        js.executeScript("arguments[0].scrollIntoView();", nav);
+        nxtbtn = driver.findElement(By.id("pnnext"));
+       
+
+        assertEquals(countA, countB, "Not equal");
+        // to do
+        localLogger.info("sup");
+    
 
         driver.quit();
     }
